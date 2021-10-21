19c91c7416268df131f961b27a5237085fae97af
RH-devs <rasheedatajiborisha@gmail.com>
2021-10-21 07:27:22 +0000

    Add function for user to select operator

--- run.py
+++ run.py
@@ -6,3 +6,14 @@ def select_operator():
     and if an incorrect option is made, the user is
     given relevant feedback.
     """
+    global option
+    option = input(
+                   "Please select an operation to perform"
+                   "your numerical activity: \n"
+                   "1 for Multiply\n"
+                   "2 for Addition\n"
+                   "3 for Subtraction\n"
+                   "4 for Modulo\n"
+                   "5 for Division\n"
+                   "6 for Power(Exponent)\n")
+    return option
