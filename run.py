aecdcb8b295dba5071f0ed417eb8c9469ba6524e
RH-devs <rasheedatajiborisha@gmail.com>
2021-10-21 06:33:10 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Simple text-based Calculator
+
+def select_operator():
+    """
+    User input is requested for operator selection,
+    and if an incorrect option is made, the user is
+    given relevant feedback.
+    """
