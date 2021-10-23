75d9b8857bb54ec56d16cd843411c15a8bdfee77
RH-devs <rasheedatajiborisha@gmail.com>
2021-10-23 06:28:17 +0000

    Add function for square root calculation

--- run.py
+++ run.py
@@ -1,35 +1,43 @@
-# Simple text-based Calculator
-
 def select_operator():
     """
     User input is requested for operator selection,
     and if an incorrect option is made, the user is
     given relevant feedback.
     """
-    while True:
-        print("The functions below will help carry out your basic calculations.")
-        print("1 for Multiply")
-        print("2 for Addition")
-        print("3 for Subtraction")
-        print("4 for Modulo")
-        print("5 for Division")
-        print("6 for Power(Exponent)")
 
-    option.str = input("Please select an operation to perform your calculation:")
-    
-    select_operator = option.str("")
-
-    if calculating_operator(option.str):
-        print(f"You selected {option}")
+    global option
+    option = input(
+                 "The functions below will help carry out your calculation:\n"
+                 "1. Addition\n"
+                 "2. Subtraction\n"
+                 "3. Multiply\n"
+                 "4. Division\n"
+                 "5. Square root\n"
+                 "6. Exit\n")
+    return option
 
 
-def calculating_operator():
+def numeric_operation():
     """
-    Based on the user's selection,it performs the appropriate computation
+    Based on the user's selection, performs the required computation
     and returns the result to the calling code.
     """
-    calculating_operator()
-  
+    select_operator()
+    NUM_CONSTANT = 0.5
+    if option == "5":
+        print("Doing the Square Root Calculation...\n")
+        sqrt_num = number_entry("1")
+        result = sqrt_num ** NUM_CONSTANT
+        print("Square Root Calculation completed Successfully.\n")
+        return result
+    else:
+        num1 = number_entry("Enter first number")
+        num2 = number_entry("Enter second number")
+        my_option = option
+        if my_option == "1":
+            print("Currently adding numbers...\n")
+            result = num1 + num2
+            return result
 
 
 def number_entry(pos):
@@ -38,19 +46,9 @@ def number_entry(pos):
     and provides relevant feedback to the user on the cause of the issue
     and how to resolve it.
     """
-    try:
-        num = float(input(f"Enter {pos} number:\n"))
-        return num
-    except ValueError:
-        print("Invalid entry - NOT a number. \n"
-                  "Please try again!")
-
-
-"""
-Receives the user name and displays a welcome message
-in relation to the user.
-"""
-user_name = input("(Optional) Input your name: \n")
-print(f"Welcome {user_name} to SimpleX Calculator")
-answer = calculating_operator()
-print(f"The Last Result of your calculation is {answer}")
+    while True:
+        try:
+            num = float(input(f"Enter {pos} number:\n"))
+            return num
+        except ValueError:
+            print("Invalid entry, Please try again!:\n")
